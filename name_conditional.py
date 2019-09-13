e841f472c91a17430289b421422db4845141bf17
amalong <amalong@gmail.com>
2019-09-13 16:59:52 -0700

    Example

---
+++ name_conditional.py
@@ -0,0 +1,8 @@
+print ("Enter the Secret Name:")
+name = input()
+if name == "Who":
+    print("Who's on first")
+elif name == "What":
+    print("What's on second.")
+else:
+    print("This could be a loop someday!")
